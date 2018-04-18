db12c6cc480cb529f9aca3a71dcf91db896b2f8b
Kiran Bhadury <kbhadury@ucdavis.edu>
2018-04-18 01:00:35 -0700

    Saving in case everything goes wrong...

--- MRTKv1/Assets/Scripts/Constants.cs
+++ MRTKv1/Assets/Scripts/Constants.cs
@@ -4,6 +4,6 @@ using UnityEngine;
 public class Constants
 {
     public static Color RED = new Color(0.8f, 0, 0);
-    public static Color YELLOW = new Color(1, 0.6f, 0);
+    public static Color YELLOW = new Color(1, 0.839f, 0.2f);
     public static Color GREEN = new Color(0, 0.7f, 0);
 }
