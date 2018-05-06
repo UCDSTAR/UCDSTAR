29076037d58b5ad547462099e4b5bc2740837d33
Kiran Bhadury <kbhadury@ucdavis.edu>
2018-05-05 22:43:07 -0700

    Updated red,yellow,green colors

--- MRTKv1/Assets/Scripts/Constants.cs
+++ MRTKv1/Assets/Scripts/Constants.cs
@@ -3,9 +3,9 @@
 public class Constants
 {
     public static Color WHITE = new Color(1, 1, 1);
-    public static Color RED = new Color(0.8f, 0, 0);
-    public static Color YELLOW = new Color(1, 0.839f, 0.2f);
-    public static Color GREEN = new Color(0, 0.7f, 0);
+    public static Color RED = new Color(0.898f, 0.247f, 0.227f);
+    public static Color YELLOW = new Color(0.961f, 0.71f, 0.102f);
+    public static Color GREEN = new Color(0.012f, 0.616f, 0.353f);
     public static Color ACTIVE_STEP = new Color(1, 1, 1); //solid white
     public static Color INACTIVE_STEP = new Color(1, 1, 1, 0.5f); //half-transparent white
 }
